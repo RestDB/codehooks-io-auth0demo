8ad5d153b4d4c52524597cc22379fb7625e7870f
Knut Martin Tornes <knutmt@gmail.com>
2022-09-25 23:29:30 +0200

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,10 @@ const Footer = () => (
   <footer className="bg-light p-3 text-center">
     <div className="logo" />
     <p>
-      Sample project provided by <a href="https://auth0.com">Auth0</a>
+      Sample project provided by <a href="https://auth0.com">Auth0</a> 
+    </p>
+    <p>
+      modified to use <a href="https://codehooks.io.com">codehooks.io</a> as an authenticated backend.
     </p>
   </footer>
 );
