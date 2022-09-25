9ee6293aa2217a86e799d43a1f615ae7f6c89bb2
Knut Martin Tornes <knutmt@gmail.com>
2022-09-25 23:26:15 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const providerConfig = {
   domain: config.domain,
   clientId: config.clientId,
   ...(config.audience ? { audience: config.audience } : null),
-  redirectUri: window.location.origin,
+  redirectUri: `${window.location.origin}/codehooks-io-auth0demo`,
   onRedirectCallback,
 };
 
