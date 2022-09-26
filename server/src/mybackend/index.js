79e98c778b6edb53e9260305917e4a67e9a20496
Knut Martin Tornes <knutmt@gmail.com>
2022-09-26 10:48:00 +0200

    removed backend localserver files

--- server/src/mybackend/index.js
+++ server/src/mybackend/index.js
@@ -1,43 +1,55 @@
 
 /*
-* Auto generated Codehooks (c) example.
+* Codehooks code for fetching user info for all routes using Auth0 authentication
 */
+
 import { app, Datastore } from 'codehooks-js'
 import fetch from 'node-fetch';
 
+// middleware to get Auth0 user info in request
+// all routes will now have access to user info in req.user
 
-// get user profile if not available and cache it
-app.use(async (req, res, next) => {
+const userProfileFromAuth0 = ({ domain }) => async (req, res, next) => {
   try {
     const { authorization } = req.headers;
     if (authorization) {
       const token = authorization.replace('Bearer ','');
       const conn = await Datastore.open();
+
+      // try to load user from codehooks.io key/value store
       const user = await conn.get(`token-${token}`, { keyspace: 'sessions'});
+
+      // found in cache?
       if (user){
-        console.log("user fetched from cache");
         req.user = JSON.parse(user);
         return next();
       }
-      console.log("fetching user from Auth0 API");
-      const resp = await fetch(`https://${process.env.AUTH0_DOMAIN}/userinfo`, {
+
+      // fetch user from Auth0 API
+      const resp = await fetch(`https://${domain}/userinfo`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       });
       req.user = await resp.json();
+
+      // store in key/value store for twenty minutes
       conn.set(`token-${token}`, JSON.stringify(req.user),{ keyspace: 'sessions', ttl: 1000 * 60 * 20}); // ttl twenty minutes
+    }
+    else {
+      return res.sendStatus(401);
     }
     next();
   } catch (error) {
     next(error);
   } 
-});
+}
+
+app.use(userProfileFromAuth0({domain: process.env.AUTH0_DOMAIN}));
 
 app.get('/hello', (req, res) => {
-  console.log("hello");
-  //res.json({"message": "hello"});
-  res.json({"message": "hello " + req.user.nickname, user: req.user});
+  const nickname = (req.user && req.user.nickname) || 'anonymous';
+  res.json({"message": `Hello ${nickname}`, user: req.user, now: new Date()});
 });
 
 
